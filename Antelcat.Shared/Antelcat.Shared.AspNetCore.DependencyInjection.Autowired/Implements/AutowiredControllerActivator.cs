14c1ed2b04c087d27fe741264ec81ac2da04e5fb
Feast <feast107@gmail.com>
2023-05-31 11:18:06 +0800

    Add

---
+++ Antelcat.Shared/Antelcat.Shared.AspNetCore.DependencyInjection.Autowired/Implements/AutowiredControllerActivator.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Antelcat.Implements.Services;
+
+public abstract class ControllerActivatorBase<TServiceProvider> : IControllerActivator 
+    where TServiceProvider : IServiceProvider
+{
+    protected abstract TServiceProvider ProvideService(IServiceProvider provider);
+    public object Create(ControllerContext? context)
+    {
+        if (context == null)
+            throw new ArgumentNullException($"{nameof(ControllerContext)} is null");
+        var provider = context.HttpContext.RequestServices;
+        if (provider is not TServiceProvider)
+            provider = ProvideService(context.HttpContext.RequestServices);
+        return provider.GetRequiredService(context.ActionDescriptor.ControllerTypeInfo.AsType());
+    }
+    public void Release(ControllerContext? context, object controller)
+    {
+        if (context == null) throw new ArgumentNullException($"{nameof(ControllerContext)} is null");
+        switch (controller)
+        {
+            case null:
+                throw new ArgumentNullException(nameof(controller));
+            case IDisposable disposable:
+                disposable.Dispose();
+                break;
+        }
+    }
+}
+
+public class AutowiredControllerActivator<TAttribute> 
+    : ControllerActivatorBase<TransientAutowiredServiceProvider<TAttribute>> 
+    where TAttribute : Attribute
+{
+    protected override TransientAutowiredServiceProvider<TAttribute> ProvideService(
+        IServiceProvider provider) =>
+        new(provider);
+}
