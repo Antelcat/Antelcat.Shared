af0661b8cfc908f69fb1d53c666a42baa5a0fc56
Feast <feast107@gmail.com>
2023-05-31 09:45:40 +0800

    Create LinqExtension.cs

---
+++ Antelcat.Shared/Antelcat.Shared.Extensions.LinqExtension/LinqExtension.cs
@@ -0,0 +1,9 @@
+﻿namespace Antelcat.Extensions;
+
+public static class LinqExtension
+{
+    public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+    {
+        foreach (var item in source) action(item);
+    }
+}
