fba01224c0c122ff63ef8871e66ec25bf5cc1138
Feast <feast107@gmail.com>
2023-05-31 17:32:54 +0800

    Update

---
+++ Antelcat.Shared/Antelcat.Shared.NET Framework/Usings.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Antelcat.Shared.NET_Framework
+{
+    internal class Usings
+    {
+        public static void Using()
+        {
+        }
+    }
+}
