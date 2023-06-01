7ef7abb08061fa4023bb1f3e73e4f3f2d81efcec
Feast <feast107@gmail.com>
2023-06-01 10:31:06 +0800

    Update Testing

---
+++ src/Test/.NET Standard/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Antelcat.Shared.Test.NET_Standard
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
